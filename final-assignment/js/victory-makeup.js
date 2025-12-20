62ec8d9f95937ba07e6fc54d72f931d9c6e4a538
lochleaf <200536258@student.georgianc.on.ca>
2025-12-19 23:07:16 -0500

    finished the rest of the letters

--- final-assignment/js/victory-makeup.js
+++ final-assignment/js/victory-makeup.js
@@ -12,18 +12,3 @@ closeBtn.addEventListener("click", function () {
     overlay.classList.add("hidden");
     
 });
-
-
-/*const  = document.querySelector(".placenum form");
-const overlay = document.getElementById("alertOverlay");
-const closeBtn = document.getElementById("closeAlert");
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    overlay.classList.remove("hidden");
-});
-
-closeBtn.addEventListener("click", function () {
-    overlay.classList.add("hidden");
-    
-});*/
