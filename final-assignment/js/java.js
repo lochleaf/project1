e4f30fd9be6140a7afc3bee6bb9f19dfd91a6fb6
lochleaf <200536258@student.georgianc.on.ca>
2025-12-19 22:28:39 -0500

    added js and reset and fix
    added new js file for alert script so on click when submit alert is activeted and also added reset button to refresh page

---
+++ final-assignment/js/java.js
@@ -0,0 +1,29 @@
+
+const form = document.querySelector(".placenum form");
+const overlay = document.getElementById("alertOverlay");
+const closeBtn = document.getElementById("closeAlert");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    overlay.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", function () {
+    overlay.classList.add("hidden");
+    
+});
+
+
+/*const  = document.querySelector(".placenum form");
+const overlay = document.getElementById("alertOverlay");
+const closeBtn = document.getElementById("closeAlert");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    overlay.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", function () {
+    overlay.classList.add("hidden");
+    
+});*/
