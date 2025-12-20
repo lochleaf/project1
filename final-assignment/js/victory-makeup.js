da85766e2df56b6e040303d5d73ab43759b8b980
lochleaf <200536258@student.georgianc.on.ca>
2025-12-20 01:09:44 -0500

    commented code and fix and clean up

--- final-assignment/js/victory-makeup.js
+++ final-assignment/js/victory-makeup.js
@@ -1,14 +1,22 @@
 
+/*selects the form inside placenum section*/
 const form = document.querySelector(".placenum form");
+/*selects alert overlay background*/
 const overlay = document.getElementById("alertOverlay");
+/*selects close button inside alert box*/
 const closeBtn = document.getElementById("closeAlert");
 
+/*listens for form submission*/
 form.addEventListener("submit", function (event) {
+    /*stops page from refreshing*/
     event.preventDefault();
+    /*shows alert overlay*/
     overlay.classList.remove("hidden");
 });
 
+/*click on close button*/
 closeBtn.addEventListener("click", function () {
+    /*hides alert overlay*/
     overlay.classList.add("hidden");
     
 });
